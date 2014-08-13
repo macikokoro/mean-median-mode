11d134888781dae1266b50466f8210dcc7a8d24d
Joaquin <betwinsouls@gmail.com>
2014-08-13 09:26:46 -0700

    update to main js file to account for no mode

--- meeny-miny-mo.js
+++ meeny-miny-mo.js
@@ -1,8 +1,7 @@
-
 //Put it all together
 
 //Mean=============================
-var numArray = [2, 4, 4, 5, 5, 6, 8, 10, 11];
+var numArray = [2, 4, 5, 6, 8, 10, 11];
 console.log("The array is: " + numArray);
 var mean = 0;
 
@@ -34,14 +33,24 @@ function findMedian() {
 findMedian();
 
 //Mode ===================================
+//empty array to store results
 var result = [];
 //loop through array
-for ( i = 0; i < numArray.length; i++) {
+for (var i = 0; i < numArray.length; i++) {
     //compare results
     if (numArray[i + 1] === numArray[i]) {
         //push duplicates
         result.push(numArray[i]);
     }
 }
+//function to eliminate duplicates
+var unique = result.filter(function (elem, index, self) {
+    return index == self.indexOf(elem);
+});
 
-console.log("**** The mode is: " + result);
+if (result.length < 1) {
+    console.log("no mode!");
+} else {
+    //log the repeating numbers in array
+    console.log("*** The mode: " + unique);
+}
